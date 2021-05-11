b66dddb2c92819ee9e717bc77798483009bd706a
mqlabc <15854859214@163.com>
2021-05-11 13:02:36 +0800

    modify test.py to discover 'tests' dir when working in project root dir.

--- app/test.py
+++ app/test.py
@@ -4,5 +4,5 @@
 import unittest
 
 if __name__ == '__main__':
-    tests = unittest.TestLoader().discover('tests')
+    tests = unittest.TestLoader().discover('app/tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
